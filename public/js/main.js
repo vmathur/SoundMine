e2f9eeed925f2e381abab163f97a60bfdbb69b3c
Noorain Noorani <noorain.noorani@gmail.com>
2014-07-05 17:30:34 -0400

    including firebase, adding facebook authentication

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,8 @@ var container = $('.container'),
 	player = $('.player'), 
 	play = $('#play'),
 	pause = $('#pause'),
-	song = new Audio('../music/lights.mp3');
+	song = new Audio('../music/lights.mp3'),
+	_user;
 
 //firebase references
 var appRef = new Firebase('https://shining-fire-9992.firebaseio.com/'),
@@ -13,19 +14,19 @@ var auth = new FirebaseSimpleLogin(appRef, function(error, user) {
 	if ( user ) {
 		$('h2').text('Welcome ' + user.displayName +'!'); //welcome our user
 		
-		userRef = playerRef.child(user.id); //setup gloabl db
-		userRef.child('displayName').set(user.displayName); //add user to firebase
+		myRef = usersRef.child(user.id); //setup gloabl db
+		myRef.child('displayName').set(user.displayName); //add user to firebase
 		_user = user; //for reference in other places
 		
 		manageConnection(user); //online status
-		userRef.on('value', showBoard); //show the board when playing
+		myRef.on('value', manageListens); //manage my music preferences
 	}
 });
 
 //sets online status to true/false on connect/disconect
 function manageConnection(user) {
-	onlineRef = userRef.child('online'),
-	connectedRef = new Firebase('https://blinding-fire-6122.firebaseio.com/.info/connected');
+	onlineRef = myRef.child('online'),
+	connectedRef = new Firebase('https://shining-fire-9992.firebaseio.com/.info/connected');
 	connectedRef.on('value', function(snap) {
 	  if (snap.val() === true) {
 	    var con = onlineRef.set(true);
@@ -57,6 +58,10 @@ function playSongById(songId){
 
 function saveToRef(timestamp, songId) {
 
+}
+
+function manageListens() {
+	console.log('managing what i listen to');
 }
 
 auth.login('facebook');
